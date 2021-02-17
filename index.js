3e3d8bb3ba62f0cdb46f3770e1b482ae42a3dba1
Avinash <biradarav.100@gmail.com>
2021-02-17 14:39:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+var randomNumber1=Math.floor(Math.random()*6+1);
+
+var randomDiceImage="dice"+randomNumber1+".png";
+
+var randomImageSource="images/"+randomDiceImage;
+
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
+
+var randomNumber2=Math.floor(Math.random()*6+1);
+
+var randomImageSource2="images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins !";
+}
+
+else if(randomNumber1<randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins ! 🚩";
+}
+
+else
+{
+    document.querySelector("h1").innerHTML="Match Draw!";
+}
+
+
+function reload()
+{
+    reload=location.reload();
+}
+
+document.querySelector("#reload").reloadButton.addEventListener("click",reload);
